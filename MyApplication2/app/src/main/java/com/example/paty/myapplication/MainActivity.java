339eeb099d2a26eca46c62ca8bad0e81af5bc1e5
Patricia Flores <silpatflogan@gmail.com>
2016-02-24 01:19:10 -0600

    New activity created, and menu activity switch

--- MyApplication2/app/src/main/java/com/example/paty/myapplication/MainActivity.java
+++ MyApplication2/app/src/main/java/com/example/paty/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.paty.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -73,9 +74,15 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            showSettings();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void showSettings() {
+        Intent intent = new Intent(MainActivity.this, Settings.class);
+        startActivity(intent);
     }
 }
